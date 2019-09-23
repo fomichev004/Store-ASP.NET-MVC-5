6d51dbfad4f17d68803cb95e6ab0e49c51060531
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-09-23 16:23:47 +0300

    Update PagesController.cs

--- my-store-project/Areas/Admin/Controllers/PagesController.cs
+++ my-store-project/Areas/Admin/Controllers/PagesController.cs
@@ -28,9 +28,41 @@ namespace my_store_project.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+         [HttpGet]
         public ActionResult AddPage()
         {
             return View();
         }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+         public ActionResult AddPage(PageVM model)
+        {
+            //Проверка модели на валидность
+            if (!ModelState.isValid)
+            {
+                return View(model);
+            }
+
+            //Объявляем переменную для краткого описания (slug)
+
+            //Инициализируем класс PageDTO
+
+            //Присваеваем заголовок модели
+
+            //Проверяем, есть ли краткое описание, если нет, присваиваем его
+
+            //Убеждаемся, что заголовок и краткое описание - уникальны
+
+            //Присваиваем оставшиеся значения модели
+
+            //Сохраняем модель в базу данных
+
+            //Передаем сообщение через TempData
+
+            //Переадресовываем пользователя на метод INDEX
+
+        }
+
     }
 }
