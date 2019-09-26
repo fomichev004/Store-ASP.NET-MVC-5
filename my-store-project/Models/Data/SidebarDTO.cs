21ca67abcbace87cc83f501b19a6535e22bb3bb5
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-09-26 16:50:48 +0300

    Create SidebarDTO.cs

---
+++ my-store-project/Models/Data/SidebarDTO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace my_store_project.Models.Data
+{
+    [Table("tblSidebar")]
+    public class SidebarDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Body { get; set; }              
+    }
+}
