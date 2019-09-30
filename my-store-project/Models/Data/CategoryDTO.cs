b5821fca92b79cc8e3ea56249a4ea36e235621e6
fomichev004 <fomichev004@gmail.com>
2019-09-30 22:26:51 +0300

    Create CategoryDTO.cs

---
+++ my-store-project/Models/Data/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+//Класс контекста данных для категорий
+namespace my_store_project.Models.Data
+{
+    [Table("tblCategories")]
+    public class CategoryDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+
+    }
+}
