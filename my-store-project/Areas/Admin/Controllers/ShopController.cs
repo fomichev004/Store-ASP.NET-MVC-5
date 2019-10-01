6ee6c3d7761643fd8e65becfdc7c5097d122aa75
fomichev004 <fomichev004@gmail.com>
2019-10-01 21:32:47 +0300

    Update ShopController.cs

--- my-store-project/Areas/Admin/Controllers/ShopController.cs
+++ my-store-project/Areas/Admin/Controllers/ShopController.cs
@@ -34,8 +34,8 @@ namespace my_store_project.Areas.Admin.Controllers
         	using (Db db = new Db())
         	{
         		//Проверяем имя категории на уникальность
-        		if (db.Categories.Any)(x=> x.Name ==catName))
-					return "tittletaken"; 		
+        		if (db.Categories.Any(x => x.Name == catName))
+                    return "tittletaken"; 		
 
 	        	//Инициализируем модель DTO
 	        	CategoryDTO dto = new CategoryDTO();
@@ -54,6 +54,29 @@ namespace my_store_project.Areas.Admin.Controllers
         	}
         	//Возвращаем ID в представление
         	return id;
+        }
+
+        //POST: Admin/Shop/ReorderCategories  
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Реализуем начальный счетчик
+                int count = 1;
+
+                //Инициализируем модель данных
+                CategoryDTO dto;
+
+                //Устанавливаем сортировку для каждой страницы
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
         }
     }
 }
