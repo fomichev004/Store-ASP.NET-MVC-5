0d66807cc90b8def36625addc33a9a5e241f1c72
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-10-03 15:48:42 +0300

    Update ShopController.cs

--- my-store-project/Areas/Admin/Controllers/ShopController.cs
+++ my-store-project/Areas/Admin/Controllers/ShopController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
+using System.IO;
 using System.Web.Mvc;
 
 namespace my_store_project.Areas.Admin.Controllers
@@ -176,33 +178,107 @@ namespace my_store_project.Areas.Admin.Controllers
             //Инициализируем и сохраняем модель на основе ProductDTO
             using (Db db = new Db())
             {
-                ProductDTO
+                ProductDTO product = new ProductDTO();
+
+                product.Name = model.Name;
+                product.Slug = model.Name.Replace(" ", "-").ToLower();
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.CategoryId = model.CategoryId;
+
+                CategoryDTO catDTO = db.FirstOrDefault( x => x.Id == model.CategoryId);
+                product,CategoryName = catDTO.Name;
+
+                db.Product.Add(product);
+                db.SaveChanges();
+
+                id = product.Id;
             }
                 //Добавляем сообщение в TempData
+            	TempData["Successful message"] = "You have added a product!";
+
                 #region Upload Image
+
                 //Создаем необходимые ссылки на дериктории
+                var originalDirectory = new DirectoryInfo (string.Format($"{Server.MapPath(@"\")}Images\\Uploads"));
+
+                var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+                var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+                var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\ + id.ToString() + \\Thumbs");
+                var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\ + id.ToString() + \\Gallery");
+                var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\ + id.ToString() + \\Gallery\\Thumbs");
+
                 //Проверяем наличие директорий (если нет, создаём)
-                //Проверяем, был ли файл загруже
-                //Проверяем расширение файла
-                //Объявляем переменную с именем изображения
-                //Сохраняем имя изображения в модель DTO
-                //Назначаем пути к оригинальному и уменьшенному изображению
-                //Сохраняем оригинальное изображение
-                //Создаем и сохраняем уменьшенную копию
+                if (!Directory.Exists(pathString1))
+               		Directory.CreateDirectory(pathString1);
+
+               	if (!Directory.Exists(pathString2))
+               		Directory.CreateDirectory(pathString2);
+
+               	if (!Directory.Exists(pathString3))
+               		Directory.CreateDirectory(pathString3);
+
+               	if (!Directory.Exists(pathString4))
+               		Directory.CreateDirectory(pathString4);
+
+               	if (!Directory.Exists(pathString5))
+               		Directory.CreateDirectory(pathString5);
+
+                //Проверяем, был ли файл загружн
+               	if (file != Null && file.ContentLenght > 0)
+               	{
+               		//Получаем расширение файла
+               		string ext = file.ContentType.ToLower();
+
+               		//Проверяем расширение файла
+               		if (ext != "image/jpg" && 
+               			ext != "image/jepg" &&
+               			ext != "image/pjepg" &&
+               			ext != "image/gif" &&
+               			ext != "image/png" &&
+               			ext != "image/x-png")
+               		{
+               			using (Db db = new Db())
+               			{
+               				model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name")
+               				ModelState.AddModelError("", "The image was not uploaded - wrong image extentsion!")
+               				return View(model);
+               			}
+               		}
+               	
+                
+	                //Объявляем переменную с именем изображения
+	                string imageName = file.FileName;
+
+	                //Сохраняем имя изображения в модель DTO
+	                using (Db db = new Db())
+	                {
+	                	ProductDTO dto = db.Products.Find(id);
+	                	dto.ImageName = imageName;
+
+	                	db.SaveChanges();
+	                }
+
+	                //Назначаем пути к оригинальному и уменьшенному изображению
+	                var path = string.Format($"{pathString2}\\{imageName}"); //путь к оригинальному изображению
+	                var path2 = string.Format($"{pathString3}\\{imageName}"); //путь к уменьшенному изображению
+
+	                //Сохраняем оригинальное изображение
+	                file.SaveAs(path);
+
+	                //Создаем и сохраняем уменьшенную копию
+	                WebImage img = new WebImage(file.InputStream);
+	                img.Resize(200, 200);
+	                img.Save(path2);
+       			 }
+
                 #endregion
                 // переадресовываем пользователя
-                return RedirectToAction("AddProduct");
+                return RedirectToAction("AddProduct");          
         }
 
 
 
-        //            using (Db db = new Db())
-        //            {
-        //                //Объявляем переменную ProductId
-        //                int ProductId;
-
-        //                //Инициализируем класс PageDTO
-        //                ProductDTO dto = new ProductDTO();
 
         //                //Присваеваем заголовок модели
         //                dto.Title = model.Title.ToUpper();
