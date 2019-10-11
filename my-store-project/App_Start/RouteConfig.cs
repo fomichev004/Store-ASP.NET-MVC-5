6dce418897e5cc8f5ea24aa8f31ff1748cfa3ca2
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-10-11 16:49:31 +0300

    Update RouteConfig.cs

--- my-store-project/App_Start/RouteConfig.cs
+++ my-store-project/App_Start/RouteConfig.cs
@@ -12,11 +12,23 @@ namespace my_store_project
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute("PagesMenuPartial", "{Pages/PagesMenuPartial}", new {controller = "Pages", action = "Index"}, new []{"PagesMenuPartial"});
             
-            routes.MapRoute("Pages", "{page}", new {controller = "Pages", action = "Index"}, new []{"my_store_project.Controllers"});
-            routes.MapRoute("Default", "", new {controller = "Pages", action = "Index"}, new []{"my_store_project.Controllers"});
+            // 18
+            routes.MapRoute("SidebarPartial", "{Pages/SidebarPartial}", new {controller = "Pages", action = "SidebarPartial"}, 
+                new []{"my_store_project.Controllers"});
+                
+             routes.MapRoute("Shop", "Shop/{action}/{name}", new {controller = "Shop", action = "Index", name = UrlParameter.Optional }, 
+                new []{"my_store_project.Controllers"});
+
+            routes.MapRoute("PagesMenuPartial", "{Pages/PagesMenuPa  rtial}", new {controller = "Pages", action = "PagesMenuPartial"}, 
+                new []{"my_store_project.Controllers"});
+            
+            routes.MapRoute("Pages", "{page}", new {controller = "Pages", action = "Index"}, 
+                new []{"my_store_project.Controllers"});
+
+            routes.MapRoute("Default", "", new {controller = "Pages", action = "Index"}, 
+                new []{"my_store_project.Controllers"});
+
             // routes.MapRoute(
             //     name: "Default",
             //     url: "{controller}/{action}/{id}",
