efe25a324ef73e92540c94e8294983cddeaecfc9
fomichev004 <fomichev004@gmail.com>
2019-10-16 21:37:20 +0300

    Update CartVM.cs

--- my-store-project/Models/ViewModels/Cart/CartVM.cs
+++ my-store-project/Models/ViewModels/Cart/CartVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace my_store_project.Models.ViewModels.Pages
+namespace my_store_project.Models.ViewModels.Cart
 {
     public class CartVM
     {
