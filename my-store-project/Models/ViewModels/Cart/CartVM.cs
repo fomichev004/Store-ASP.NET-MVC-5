8f88b388338f65d4c4bc3762634ba6a388e921ed
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-10-16 11:11:46 +0300

    Create CartVM.cs

---
+++ my-store-project/Models/ViewModels/Cart/CartVM.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace my_store_project.Models.ViewModels.Pages
+{
+    public class CartVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total 
+        { 
+            get { return Quantity * Price; } 
+        }
+        public string Image { get; set; }
+    }
+}
