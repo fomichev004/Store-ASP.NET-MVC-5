ebd5c6bc91d7a06fe380ed6e68fd7f080232a4f4
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-10-24 11:51:17 +0300

    Create UserVM.cs

---
+++ my-store-project/Models/ViewModels/Account/UserVM.cs
@@ -0,0 +1,44 @@
+using my_store_project.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_store_project.Models.ViewModels.Account
+{
+    public class UserVM
+    {   
+        public UserVM()
+        {
+        }
+
+        public UserVM(UsertDTO row)
+        {
+            Id = row.Id;
+            FirstName = row.FirstName;
+            LastName = row.LastName;
+            EmailAdress = row.EmailAdress;
+            Username = row.Username;
+            Password = row.Password;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAdress)]
+        public string EmailAdress { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }  
+    }
+}
+ 
