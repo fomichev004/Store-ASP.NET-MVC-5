ee318d66172589128eff52c90bdbdbda91784eb6
fomichev004 <fomichev004@gmail.com>
2019-10-28 20:53:05 +0300

    Update AccountController.cs

--- my-store-project/Controllers/AccountController.cs
+++ my-store-project/Controllers/AccountController.cs
@@ -26,6 +26,67 @@ namespace my_store_project.Controllers
             return View("CreateAccount");
         }
 
+        // POST: account/create-account        
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            //проверяем модель на валидность
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+            if(!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Password do not match");
+                return View("CreateAccount", model);
+            }
+
+            //проверяем соответсвие пароля
+            using (Db db = new Db())
+            {
+                //проверяем имя на уникальность
+                if (db.Users.Any(x => x.Username.Equals(model.Username)))
+                {
+                    ModelState.AddModelError("", $"Username {model.Username} is taken.");
+                    model.Username = "";
+                    return View("CreateAccount", model);
+                }
+
+                //создаем экземпляр класса UserDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                //добавляем данные в модель
+                db.Users.Add(userDTO);
+
+                //сохраняем данные
+                db.SaveChanges();
+
+                //добавляем роль пользователю
+                int id = userDTO.Id;
+                UserRoleDTO userRoleDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+                db.UserRoles.Add(userRoleDTO);
+                db.SaveChanges();
+            }
+
+            //записать сообщение в TempData
+            TempData["Successful message"] = "You are nowregistered and can login.";
+
+            //переадресовываем пользователя
+            return RedirectToAction("Login");
+        }
+
         //GET: Account/Login
         [HttpGet]
         public ActionResult Login()
@@ -39,6 +100,14 @@ namespace my_store_project.Controllers
             //возвращаем представление
             return View();
         }
-        
+
+        //POST: Account/Login
+        [HttpPost]
+        public ActionResult Login(LoginUserVM model)
+        {
+            //проверяем модель на валидность
+            //проверяем пользователя на валидность
+            return View();
+        }
     }
 }
