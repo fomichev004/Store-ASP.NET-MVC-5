d1d2b9c001edca451242c303883a25407a3b5c64
fomichev004 <fomichev004@gmail.com>
2019-10-29 21:59:40 +0300

    Update Configuration.cs

--- my-store-project/Migrations/Configuration.cs
+++ my-store-project/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace my_store_project.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(my_store_project.Models.Data.Db context)
